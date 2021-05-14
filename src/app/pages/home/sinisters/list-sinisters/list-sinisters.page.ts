606d1f48de2dd8ebfbe102aecf7ed37d808430c8
nandopec <nandopec.23@gmail.com>
2021-05-14 12:22:11 -0500

    Feature: Page 'list sinisters' added

---
+++ src/app/pages/home/sinisters/list-sinisters/list-sinisters.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'agt-list-sinisters',
+  templateUrl: './list-sinisters.page.html',
+  styles: [
+  ]
+})
+export class ListSinistersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
