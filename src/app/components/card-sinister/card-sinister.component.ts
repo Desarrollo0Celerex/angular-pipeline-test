82810d6140ed627be3b6880ae67aa9dcbd7be705
nandopec <nandopec.23@gmail.com>
2021-05-19 13:00:19 -0500

    Update: Component 'card sinister' updated to show contact data

--- src/app/components/card-sinister/card-sinister.component.ts
+++ src/app/components/card-sinister/card-sinister.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Sinister } from '@interfaces/sinister.interface';
 
 declare var PopoverPlugin: any;
+declare var TooltipPlugin: any;
 
 @Component({
   selector: 'agt-card-sinister',
@@ -12,12 +13,23 @@ declare var PopoverPlugin: any;
 })
 export class CardSinisterComponent implements OnInit {
     @Input() sinister: Sinister | null = null;
+    @Output() showContactData: EventEmitter<string> = new EventEmitter<string>();
     @Output() showSinisterDetails: EventEmitter<Sinister> = new EventEmitter<Sinister>();
 
     constructor() { }
 
     ngOnInit(): void {
+        TooltipPlugin.init();
         PopoverPlugin.init();
+    }
+
+    /**
+     * Click event to request show the contact data
+     */
+    onClickShowContactData(): void {
+        if(!!this.sinister) {
+            this.showContactData.emit(this.sinister.contactId);
+        }
     }
 
     /**
