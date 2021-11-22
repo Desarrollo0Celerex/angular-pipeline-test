95214a501e3653491dee6dcc7d2e62aa582bea3d
nandopec <nandopec.23@gmail.com>
2021-11-22 17:25:21 -0600

    Update: Updated dropdown phone codes and shor name

--- src/app/components/workspace-info/workspace-info.service.ts
+++ src/app/components/workspace-info/workspace-info.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 
 import { HttpResponse } from '@interfaces/http-response.interface';
 import { Workspace } from '@interfaces/workspace.interface';
+import { WorkspaceUser } from '@interfaces/workspace-user.interface';
+import { AuthService } from '@services/auth.service';
 import { WorkspaceService } from '@services/workspace.service';
+import { WorkspaceUserService } from '@services/workspace-user.service';
 
 @Injectable()
 export class WorkspaceInfoService {
-    workspace: Workspace | null;
+    user: WorkspaceUser | null = null;
+    workspace: Workspace | null = null;
 
-    constructor(private _workspaceService: WorkspaceService) {
-        this.workspace = null;
-    }
+    constructor(
+        private _authService: AuthService,
+        private _workspaceService: WorkspaceService,
+        private _workspaceUserService: WorkspaceUserService
+    ) { }
 
     /**
      * Load the workspace data
@@ -19,6 +25,17 @@ export class WorkspaceInfoService {
         const fields: string = 'workspaceId,brandName,avatarUrl';
         this._workspaceService.getWorkspace(fields).subscribe( (res: HttpResponse) => {
             this.workspace = res.data;
+        })
+    }
+
+    /**
+     * Load de workspace user
+     */
+    loadWorkspaceUser(): void {
+        const userId: string = this._authService.userId;
+        const fields: string = 'shortName';
+        this._workspaceUserService.getWorkspaceUser(userId, fields).subscribe( (res: HttpResponse) => {
+            this.user = res.data;
         })
     }
 }
