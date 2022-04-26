5643cd02417b83346d14e9154433c8d465ad1ed7
nandopec <nandopec.23@gmail.com>
2022-04-26 17:32:02 -0500

    Feature: Deleted 'update policy' page

--- src/app/constants/routes-name.ts
+++ src/app/constants/routes-name.ts
@@ -99,7 +99,6 @@ const HOME_POLICIES_ROUTES = {
     createPolicy: (contactId: string) => `workspace/policies/create-policy/${contactId}`,
     uploadPolicy: (contactId: string, policyId: string) => `workspace/policies/upload-policy/${contactId}/${policyId}`,
     completePolicy: (contactId: string, policyId: string) => `workspace/policies/complete-policy/${contactId}/${policyId}`,
-    updatePolicy: (contactId: string, policyId: string) => `workspace/policies/update-policy/${contactId}/${policyId}`,
     endorsePolicy: (contactId: string, policyId: string) => `workspace/policies/endorse-policy/${contactId}/${policyId}`,
     cancelPolicy: (contactId: string, policyId: string) => `workspace/policies/cancel-policy/${contactId}/${policyId}`,
     showHistoryPolicy: (contactId: string, policyId: string) => `workspace/policies/history-policy/${contactId}/${policyId}`,
