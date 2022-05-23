ef7c1863279b8e04beb9d385dbff467c78d85cb2
nandopec <nandopec.23@gmail.com>
2022-05-23 10:56:54 -0500

    Update: Added a coment

--- src/app/components/card-partner-annual-wallet/card-partner-annual-wallet.service.ts
+++ src/app/components/card-partner-annual-wallet/card-partner-annual-wallet.service.ts
@@ -8,6 +8,6 @@ export class CardPartnerAnnualWalletService {
     constructor(private _partnerService: PartnerService) { }
 
     loadPartnerAnnualWallet(partnerId: number, year: number): void {
-        this._partnerService
+        //this._partnerService
     }
 }
