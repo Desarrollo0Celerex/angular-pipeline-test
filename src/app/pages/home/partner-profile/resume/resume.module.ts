13dc6212d17549d99d55c89387b3a1c588c4b145
nandopec <nandopec.23@gmail.com>
2022-05-24 17:43:02 -0500

    Feature: Added card partner global balance component

--- src/app/pages/home/partner-profile/resume/resume.module.ts
+++ src/app/pages/home/partner-profile/resume/resume.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CardPartnerActiveCoveragesModule } from '@components/card-partner-active-coverages/card-partner-active-coverages.module';
+import { CardPartnerGlobalBalanceModule } from '@components/card-partner-global-balance/card-partner-global-balance.module';
 import { CardPartnerPreferredInsurersModule } from '@components/card-partner-preferred-insurers/card-partner-preferred-insurers.module';
 import { CardPartnerWalletGlobalModule } from '@components/card-partner-wallet-global/card-partner-wallet-global.module';
 import { CardPartnerWalletProjectionModule } from '@components/card-partner-wallet-projection/card-partner-wallet-projection.module';
@@ -15,6 +16,7 @@ import { ResumePage } from './resume.page';
   ],
   imports: [
     CardPartnerActiveCoveragesModule,
+    CardPartnerGlobalBalanceModule,
     CardPartnerPreferredInsurersModule,
     CardPartnerWalletGlobalModule,
     CardPartnerWalletProjectionModule,
