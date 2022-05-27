f31dcf9a33a68934603e61732ae8f5b9f9438e96
nandopec <nandopec.23@gmail.com>
2022-05-26 19:21:31 -0500

    Update: Updated 'card payments and renewals report' components

--- src/app/components/card-partner-payment-reports/card-partner-payment-reports.component.ts
+++ src/app/components/card-partner-payment-reports/card-partner-payment-reports.component.ts
@@ -30,6 +30,56 @@ export class CardPartnerPaymentReportsComponent implements OnChanges {
         }
     }
 
+    get canDownloadReport(): boolean {
+        let canDownloadReport: boolean = true;
+        switch(this.model.selectedReportType) {
+            case this.model.REPORT_TYPES.APPLIED_PAYMENTS:
+                canDownloadReport = (this.model.totalPartnerAppliedPayments === 0) ? false : true;
+            break;
+            case this.model.REPORT_TYPES.PENDING_PAYMENTS:
+                canDownloadReport = (this.model.totalPartnerPendingPayments === 0) ? false : true;
+            break;
+        }
+        return canDownloadReport;
+    }
+
+    get contentTypeName(): string {
+        return (this.totalPartnerPayments === 1) ? 'Recibo' : 'Recibos';
+    }
+
+    get description(): string {
+        let description: string = '';
+        let label: string = '';
+        switch(this.model.selectedReportType) {
+            case this.model.REPORT_TYPES.APPLIED_PAYMENTS:
+                label = (this.model.totalPartnerAppliedPayments === 1) ? 'Recibo Aplicado' : 'Recibos Aplicados';
+                description = this.model.totalPartnerAppliedPayments + ' ' + label;
+            break;
+            case this.model.REPORT_TYPES.PENDING_PAYMENTS:
+                label = (this.model.totalPartnerPendingPayments === 1) ? 'Recibo Pendiente' : 'Recibos Pendientes';
+                description = this.model.totalPartnerPendingPayments + ' ' + label;
+            break;
+        }
+        return description;
+    }
+
+    get title(): string {
+        let title: string = '';
+        switch(this.model.selectedReportType) {
+            case this.model.REPORT_TYPES.APPLIED_PAYMENTS:
+                title = 'Recibos Aplicados';
+            break;
+            case this.model.REPORT_TYPES.PENDING_PAYMENTS:
+                title = 'Recibos Pendientes';
+            break;
+        }
+        return title;
+    }
+
+    get totalPartnerPayments(): number {
+        return this.model.totalPartnerAppliedPayments + this.model.totalPartnerPendingPayments;
+    }
+
     downloadReport(formatType: number): void {
         this._loadingService.show();
         this.model.downloadReport(this.partnerId, this.rangeStart, this.rangeEnd, formatType).then(() => {
