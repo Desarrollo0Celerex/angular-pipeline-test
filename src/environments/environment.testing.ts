5ae4daa5c6cc192965116a95c8cfe5b6c4349e53
nandopec <nandopec.23@gmail.com>
2022-07-09 17:50:58 -0500

    Feature: Added testing enviroment

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: true,
+    agenthosUrl: 'https://agenthos.com',
+    atomAccountUrl: 'https://account.atombits.xyz',
+    appAgenthosUrl: 'https://testing.agenthos.com',
+    apiUrl: 'https://dev.agenthos.com/v1',
+    atomScann: 'https://scanner.agenthos.com/api/v1',
+    firebaseConfig: {
+      apiKey: "AIzaSyCT6zwYtSCrpr5iXe3x4TcwCGcMUO12GzA",
+      authDomain: "atom-account-30384.firebaseapp.com",
+      databaseURL: "https://atom-account-30384-default-rtdb.firebaseio.com",
+      projectId: "atom-account-30384",
+      storageBucket: "atom-account-30384.appspot.com",
+      messagingSenderId: "1012733303319",
+      appId: "1:1012733303319:web:935df0a75025a0231c5a52"
+    }
+};
