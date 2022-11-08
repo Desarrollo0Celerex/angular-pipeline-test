573ad66d7eec42cf1621d32f0cb71a5f52bd557f
nandopec <nandopec.23@gmail.com>
2022-11-07 18:09:58 -0600

    Feature: Added new sinister event 'indemnification'

--- src/app/components/container-report-event/container-report-event.module.ts
+++ src/app/components/container-report-event/container-report-event.module.ts
@@ -5,9 +5,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownSelectPhoneCodeModule } from '@components/dropdown-select-phone-code/dropdown-select-phone-code.module';
 import { ModalSelectEvidenceModule } from '@components/modal-select-evidence/modal-select-evidence.module';
 
+import { CurrencyService } from '@services/currency.service';
+import { PaymentMethodService } from '@services/payment-method.service';
 import { SinisterService } from '@services/sinister.service';
 import { SinisterEventService } from '@services/sinister-event.service';
 import { SinisterEventTypeService } from '@services/sinister-event-type.service';
+import { SinisterResolutionService } from '@services/sinister-resolution.service';
 
 import { ContainerReportEventComponent } from './container-report-event.component';
 import { ContainerReportEventService } from './container-report-event.service';
@@ -22,6 +25,14 @@ import { ContainerReportEventService } from './container-report-event.service';
     ModalSelectEvidenceModule,
     ReactiveFormsModule
   ],
-  providers: [ContainerReportEventService, SinisterService, SinisterEventService, SinisterEventTypeService]
+  providers: [
+    ContainerReportEventService,
+    CurrencyService, 
+    PaymentMethodService,
+    SinisterService, 
+    SinisterEventService, 
+    SinisterEventTypeService,
+    SinisterResolutionService
+  ]
 })
 export class ContainerReportEventModule { }
