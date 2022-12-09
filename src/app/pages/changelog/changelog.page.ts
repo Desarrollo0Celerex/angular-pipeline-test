b10cf3ad7850a3c40feb62cc2ba01ac4216d0fae
nandopec <nandopec.23@gmail.com>
2022-12-08 18:17:42 -0600

    Feature: Added preview to upload a policy

--- src/app/pages/changelog/changelog.page.ts
+++ src/app/pages/changelog/changelog.page.ts
@@ -9,6 +9,18 @@ import { Component } from '@angular/core';
 export class ChangelogPage {
     logs: any[] = [
         {
+            version: '1.34.1',
+            launchDate: '29/11/2022',
+            changes: [
+                {
+                    title: 'Smart Wallet',
+                    items: [
+                        'Se agrego vista previa al seleccionar un archivo al cargar una póliza.'
+                    ]
+                }
+            ]
+        },
+        {
             version: '1.34.0',
             launchDate: '29/11/2022',
             changes: [
