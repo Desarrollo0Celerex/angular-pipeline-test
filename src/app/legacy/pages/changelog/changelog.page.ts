09a5e194bb029e997dddaf95404088ff95a6b262
nandopec <nandopec.23@gmail.com>
2023-05-19 11:40:08 -0600

    .

--- src/app/legacy/pages/changelog/changelog.page.ts
+++ src/app/legacy/pages/changelog/changelog.page.ts
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
 export class ChangelogPage {
     logs: any[] = [
         {
+            version: '1.46.1',
+            launchDate: '19/05/2023',
+            changes: [
+                {
+                    title: 'Authentication',
+                    items: ['Se resolvio bug en la autenticación'],
+                },
+            ],
+        },
+        {
             version: '1.46.0',
             launchDate: '19/05/2023',
             changes: [
