0c1fcd94ca18ac6ce94874fb52bac4ef257cc477
nandopec <nandopec.23@gmail.com>
2024-05-20 20:07:20 -0600

    Updated Dashboard KPI date ranges

--- src/app/legacy/components/container-workspace-kpis/container-workspace-kpis.component.ts
+++ src/app/legacy/components/container-workspace-kpis/container-workspace-kpis.component.ts
@@ -4,15 +4,14 @@ import * as moment from 'moment';
 import { RangeData } from '@interfaces/range-data.interface';
 
 @Component({
-  selector: 'agt-container-workspace-kpis',
-  templateUrl: './container-workspace-kpis.component.html',
-  styles: [
-  ]
+    selector: 'agt-container-workspace-kpis',
+    templateUrl: './container-workspace-kpis.component.html',
+    styles: [],
 })
 export class ContainerWorkspaceKpisComponent {
     rangeData: RangeData = {
         rangeStart: moment().subtract(30, 'days').format('DD/MM/YYYY'),
-        rangeEnd: moment().format('DD/MM/YYYY'),
-        rangeField: ''
-    }
+        rangeEnd: moment().add(40, 'days').format('DD/MM/YYYY'),
+        rangeField: '',
+    };
 }
