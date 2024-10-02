689753937476ba076f1bab7f7a4194493377a7e0
nandopec <nandopec.23@gmail.com>
2024-10-02 10:51:06 -0600

    Updated app version

--- src/app/features-legacy/home/components/sidebar/sidebar.component.ts
+++ src/app/features-legacy/home/components/sidebar/sidebar.component.ts
@@ -23,7 +23,7 @@ export class SidebarComponent implements OnInit {
     userWorkspacesModalComponent!: UserWorkspacesModalComponent;
     ROUTES_NAME: any = ROUTES_NAME;
     WORKSPACE_THEMES = WORKSPACE_THEMES;
-    appVersion = '1.52.14';
+    appVersion = '1.54.14';
     agenthosSupportPhone =
         environment.agenthos.support.phoneCode +
         environment.agenthos.support.phoneNumber;
