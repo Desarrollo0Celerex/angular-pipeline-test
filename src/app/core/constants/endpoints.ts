eee59be4482a973aa2e9801cc4fff3e4bfa03dda
nandopec <nandopec.23@gmail.com>
2024-11-14 21:01:10 -0600

    Added in progress task report

--- src/app/core/constants/endpoints.ts
+++ src/app/core/constants/endpoints.ts
@@ -93,6 +93,8 @@ export const TASK_ENDPOINTS: any = {
         `${environment.agenthos.apiUrl}/workspaces/${workspaceId}/tasks`,
     reportPendingTasks: (workspaceId: string) =>
         `${environment.agenthos.apiUrl}/workspaces/${workspaceId}/tasks/reports/pending`,
+    reportInProgressTasks: (workspaceId: string) =>
+        `${environment.agenthos.apiUrl}/workspaces/${workspaceId}/tasks/reports/in-progress`,
 };
 
 export const TASK_PROGRESS_STATUS_ENDPOINTS: any = {
