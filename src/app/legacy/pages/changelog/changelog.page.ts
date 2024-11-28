82a0049079f5f7572aa783d6e83df148d2244988
dante-barreda <65030485+dante-barreda@users.noreply.github.com>
2024-11-28 10:07:11 -0600

    Updated app version to v1.54.23

--- src/app/legacy/pages/changelog/changelog.page.ts
+++ src/app/legacy/pages/changelog/changelog.page.ts
@@ -8,6 +8,18 @@ import { Component } from '@angular/core';
 export class ChangelogPage {
     logs: any[] = [
         {
+            version: '1.54.23',
+            launchDate: '28/11/2024',
+            changes: [
+                {
+                    title: 'Contact',
+                    items: [
+                        'Se agregó un nuevo campo para agregar la fecha de expiración en los documentos del expediente de un contacto.',
+                    ],
+                },
+            ],
+        },
+        {
             version: '1.54.22',
             launchDate: '15/11/2024',
             changes: [
