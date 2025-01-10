e3365c899ffb0a9a22c7f4cb7bbbeb02bd7219b0
nandopec <nandopec.23@gmail.com>
2025-01-09 18:38:29 -0600

    Updated Angular App to version v18

--- src/app/core/http/api.http.ts
+++ src/app/core/http/api.http.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-    HttpClient,
-    HttpEvent,
-    HttpHeaders,
-    HttpParams,
-} from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
